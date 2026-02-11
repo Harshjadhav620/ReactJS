6d97089838a843e93462741f21503377a5a0d3a8
Harshjadhav620 <harshjadhav587@gmail.com>
2026-02-11 21:27:17 +0530

    Learnig react

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,13 @@
+import {Page} from "./components/page"
+
+
+export const App = () => {
+  return (
+    <div>App
+      <div>
+        <Page/> 
+      </div>
+    </div>
+
+  )
+}
---
+++ 01-folder/src/components/Page.jsx
@@ -0,0 +1,9 @@
+
+export const Page = () => {
+  return (
+    <div>page
+        <h1>Harsh jadhav</h1>
+    </div>
+  )
+}
+
