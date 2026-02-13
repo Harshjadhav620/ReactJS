d6a15324580cbd9c0c7136049b90da94a47dd649
Harshjadhav620 <harshjadhav587@gmail.com>
2026-02-13 18:38:44 +0530

    building card UI

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -1,4 +1,5 @@
 import { Page } from "./components/page";
+import { CiBookmark } from "react-icons/ci";
 
 
 
@@ -8,14 +9,22 @@ function App () {
       <div className="card">
         <div className="top">
           <img src="https://www.pngplay.com/wp-content/uploads/3/Amazon-Logo-Transparent-PNG.png" alt="Amazon Logo" />
-          <button>Save</button>
+          <button>Save <CiBookmark /></button>
 
           <div className="center">
-
+            <h3>Amazon <span>5 days ago</span></h3>
+            <h2>Senior UI/UX Designer</h2>
+            <div>
+              <h4>Part-time</h4>
+              <h4>senior-level</h4>
+            </div>
           </div>
 
           <div className="bottom">
-
+            <hr />
+            <span>$120/hr</span>
+            <span>San Francisco,CA</span>
+            <button>Apply now</button>
           </div>
 
         </div>
