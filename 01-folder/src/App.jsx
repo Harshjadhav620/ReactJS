654ac6f990ace6448b903c655dfaac05579b265f
Harshjadhav620 <harshjadhav587@gmail.com>
2026-02-13 16:58:19 +0530

    trying to bulid card ui project

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -4,12 +4,24 @@ import { Page } from "./components/page";
 
 function App () {
   return (
-    <>
     <div className="parent">
+      <div className="card">
+        <div className="top">
+          <img src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg" alt="Amazon Logo" />
+          <button>Save</button>
+
+          <div className="center">
+
+          </div>
+
+          <div className="bottom">
+
+          </div>
+          
+        </div>
+      </div>
     </div>
-    <Page/>
-    </>
   );
 }
 
-export default App;
+export default App; 
--- 01-folder/src/components/Page.jsx
+++ 01-folder/src/components/Page.jsx
@@ -1,9 +1,7 @@
 
 export const Page = () => {
-  const name = "Harsh";
   return (
     <div >
-      <h1 id="heading">Hello {name}</h1>
     </div>
   )
 }
