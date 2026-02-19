b45a491fa0a7d678e917c3f1f88a203ac4c264f1
Harshjadhav620 <harshjadhav587@gmail.com>
2026-02-19 11:15:45 +0530

    Making card UI

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -31,6 +31,31 @@ function App () {
             <button>Apply now</button>
           </div>
       </div>
+      <div className="card">
+          <div>
+            <div className="top">
+           <img src="https://www.pngplay.com/wp-content/uploads/3/Amazon-Logo-Transparent-PNG.png" alt="Amazon Logo" />
+           <button>Save <CiBookmark size={12} /></button>
+          </div>  
+
+          <div className="center">
+            <h3>Amazon <span>5 days ago</span></h3>
+            <h2>Senior UI/UX Designer</h2>
+            <div className="tag">
+              <h4>Part-time</h4>
+              <h4>senior-level</h4>
+            </div>
+          </div>
+          </div>
+
+          <div className="bottom">
+            <div>
+              <h3>$120/hr</h3>
+              <p>Mumbai,India</p>
+            </div>
+            <button>Apply now</button>
+          </div>
+      </div>
     </div>  
   );
 } 
