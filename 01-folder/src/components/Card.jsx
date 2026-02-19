24cc2a212de8f288d90c8b2a78d590a3a7e08635
Harshjadhav620 <harshjadhav587@gmail.com>
2026-02-19 16:00:34 +0530

    Completed the Card UI project

--- 01-folder/src/components/Card.jsx
+++ 01-folder/src/components/Card.jsx
@@ -1,30 +1,31 @@
 import { CiBookmark } from "react-icons/ci";
 
 
-export const Card = () => {
+export const Card = (props) => {
+
   return (
     <>
       <div className="card">
           <div>
             <div className="top">
-           <img src="https://www.pngplay.com/wp-content/uploads/3/Amazon-Logo-Transparent-PNG.png" alt="Amazon Logo" />
+           <img src={props.brandlogo} alt="Amazon Logo" />
            <button>Save <CiBookmark size={12} /></button>
           </div>  
 
           <div className="center">
-            <h3>Amazon <span>5 days ago</span></h3>
-            <h2>Senior UI/UX Designer</h2>
+            <h3>{props.company} <span>{props.datepost}</span></h3>
+            <h2>{props.post}</h2>
             <div className="tag">
-              <h4>Part-time</h4>
-              <h4>senior-level</h4>
+              <h4>{props.tag1}</h4>
+              <h4>{props.tag2}</h4>
             </div>
           </div>
           </div>
 
           <div className="bottom">
             <div>
-              <h3>$120/hr</h3>
-              <p>Mumbai,India</p>
+              <h3>{props.pay}</h3>
+              <p>{props.location}</p>
             </div>
             <button>Apply now</button>
           </div>
