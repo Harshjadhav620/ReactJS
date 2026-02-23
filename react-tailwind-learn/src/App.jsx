e87dc095d1d39fc6e3193b5b468a45533d4f37a2
Harshjadhav620 <harshjadhav587@gmail.com>
2026-02-23 17:08:35 +0530

    Doing some practice

--- react-tailwind-learn/src/App.jsx
+++ react-tailwind-learn/src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/Button'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className='bg-red-500'>Harsh</div>
+    <div className='mx-auto flex max-w-sm items-center gap-x-4 bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10'>
+      <img className='size-12 shrink-0' src="/img/logo.svg" alt="ChitChat Logo" />
+      <div>
+        <div class="text-xl font-medium text-black dark:text-white">ChitChat</div>
+        <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
+      </div>
+    </div>
     </>
   )
 }
--- react-tailwind-learn/src/components/Button.jsx
+++ react-tailwind-learn/src/components/Button.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <div>Button</div>
+    <div>
+      <button className='bg-gray-400 '>Add</button>
+    </div>
   )
 }
 
