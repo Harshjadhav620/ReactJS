d5765558e818cec9b4ef5c088a8d81de7b90ff9d
Harshjadhav620 <harshjadhav587@gmail.com>
2026-02-25 14:15:14 +0530

    learing to build website UI

--- react-tailwind-learn/src/App.jsx
+++ react-tailwind-learn/src/App.jsx
@@ -10,13 +10,7 @@ function App() {
 
   return (
     <>
-    <div className='mx-auto flex max-w-sm items-center gap-x-4 bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10'>
-      <img className='size-12 shrink-0' src="/img/logo.svg" alt="ChitChat Logo" />
-      <div>
-        <div class="text-xl font-medium text-black dark:text-white">ChitChat</div>
-        <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
-      </div>
-    </div>
+    <div className='h-screen w-full bg-gray-900'></div>
     </>
   )
 }
