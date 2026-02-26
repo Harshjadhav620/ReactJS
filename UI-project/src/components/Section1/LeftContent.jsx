7a1aef18e3f1ef414a69a14a9dee5b6a5bdc6056
Harshjadhav620 <harshjadhav587@gmail.com>
2026-02-26 14:31:50 +0530

    setup the rightcontent ui

--- UI-project/src/components/Section1/LeftContent.jsx
+++ UI-project/src/components/Section1/LeftContent.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 const LeftContent = () => {
   return (
-    <div className='bg-blue-600 h-full w-1/4'>
-        <div>
-            
+    <div className='bg-blue-300 h-full w-1/3 flex flex-col justify-between'>
+        <div className='p-6'>
+          <h3 className='mb-7 text-6xl font-bold'>Prospective <br /> <span>customer</span> <br /> segmentation </h3>
+          <p className='text-xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus delectus sapiente cum corporis architecto, a sint. Totam vero obcaecati sequi!</p>
         </div>
-        <div>
-
-        </div>
+      <div className='text-8xl'>
+        <i className="ri-arrow-right-up-line"></i>
+      </div>
         </div>
   )
 }
