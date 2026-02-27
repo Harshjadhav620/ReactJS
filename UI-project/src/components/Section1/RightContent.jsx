62609316a42859b46f041592c03e339f37aabcb4
Harshjadhav620 <harshjadhav587@gmail.com>
2026-02-27 19:36:11 +0530

    image proping done

--- UI-project/src/components/Section1/RightContent.jsx
+++ UI-project/src/components/Section1/RightContent.jsx
@@ -5,8 +5,8 @@ import RightCard from './RightCard'
 const RightContent = (props) => {
   return (
     <div className=' h-full w-2/3 p-6 flex gap-10 flex-nowrap overflow-x-auto '>
-      {props.users.map(function(){
-        
+      {props.users.map(function(elem){
+        return <RightCard img={elem.img} tag={elem.tag}/>
       })}
     </div>
   )
