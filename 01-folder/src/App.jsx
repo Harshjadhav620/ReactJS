e0ee760c8c817fd23d4e91a90367b9bc094fa512
Harshjadhav620 <harshjadhav587@gmail.com>
2026-03-07 11:49:57 +0530

    Learn using advance useState

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  
-  const [num, setNum] = useState(10)
-  const [user, setUser] = useState("Harsh")
 
-  function ChangeNumUser() {
-    setNum(20)
-    setUser("Aman")
+  const [num, setNum] = useState(0)
+
+  function increaseNum() {
+    setNum(num+1)
   }
+
+  function decreaseNum() {
+    setNum(num-1)
+  }
+
+  function jump5Num() {
+    setNum(num+5)
+  }
+
 
   return (
     <div>
-      <h1>Value of num is {num} <br /> Value of user is {user}</h1>
-      <button onClick={ChangeNumUser}> Click</button>
+      <h1>{num}</h1>
+      <button onClick={increaseNum}>Increase</button>
+      <button onClick={decreaseNum}>Decrease</button>
+       <button onClick={jump5Num}>Increase 5</button>
     </div>
   )
 }
