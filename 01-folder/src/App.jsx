dc827eea177e662e6db4ebbf8798ca05a7fce156
Harshjadhav620 <harshjadhav587@gmail.com>
2026-03-12 18:38:48 +0530

    Two way data binding

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [title, setTitle] = useState("")
 
   const submitHandler = (e)=> {
     e.preventDefault()
-    console.log('Form submitted')
+    console.log('Form submitted by',title)
   }
 
   return (
@@ -12,7 +13,12 @@ const App = () => {
       <form onSubmit={(e)=>{
         submitHandler(e)
       }}>
-        <input type="text" placeholder='Enter your name' />
+        <input type="text" 
+        placeholder='Enter your name'
+        value={title}
+        onChange={(e)=>{
+          setTitle(e.target.value)
+        }} />
         <button>Submit!</button>
       </form>
     </div>
